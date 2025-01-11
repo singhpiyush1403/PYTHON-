1e49535215788813b897ad86e3e9d401c99a9853
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-01-11 16:18:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+
+
+a =  int(input(" number:"))
+b= int(input("number:"))
+print("sum =", a+b)
+
+
+1.  input 2 number print their sum
+
+a=input("type a number : ")
+b= input("type b number :")
+sum = int(a)+ int (b)
+print(" sum is: ", sum)
+
+2.  input two floating point number and print their sum
+
+a = float(input(" number is  first :"))
+b = float(input("number is second : "))
+print("avg= ", (a+b)/2)
+
+input side of square and print area
+
+side=float(input("side square is :"))
+print("area=",side*side)
+
+
+3. input 2 int number a, b print true if a greater than or equal to b. if not ptint false.
+
+a= int(input(" enter first: "))
+b=int (input("enter second: "))
+print(a>=b)
+
+
+
